22ae118ced002af323efa884a706a3529d3e05c6
Adrian Cooney <cooney.adrian@gmail.com>
2015-12-01 16:13:18 +0000

    Script to quickly dump the db.

---
+++ scripts/db_dump.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DATE=`date +%d%m%Y`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR/../data/dumps/"
+echo "Dumping database exam_papers.."
+pg_dump exam_papers -C -O -c -x > "exam_papers-$DATE.sql"
